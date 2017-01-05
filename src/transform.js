ccee8503c6ef1f46c5cb6507b29cd9d017923a55
joaker <jooakley@gmail.com>
2017-01-05 18:02:31 -0500

    fix arrow function typo

--- src/transform.js
+++ src/transform.js
@@ -1,5 +1,10 @@
+import {defaultDelimiter, delimiters, columnTypes, columnLabels} = './constants';
 
-export const separator = arg => input => {/* TODO implement */};
+export const separator = (delim = defaultDelimiter) => input => {
+  if(!input) return [];
+  const parts = input.split(delim);
+  return parts;
+};
 export const convertType = arg => input => {/* TODO implement */};
 export const labeler = arg => input => {/* TODO implement */};
 export const composeTransforms = arg => input => {/* TODO implement */};
